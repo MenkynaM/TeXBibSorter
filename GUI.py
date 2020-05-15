0ae1f38bee315f2b894b516db809330797d780cb
Martin Menkyna <menkyna.m@gmail.com>
2020-05-15 22:36:48 +0300

    Add first GUI

--- GUI.py
+++ GUI.py
@@ -1,3 +1,35 @@
 import tkinter as tk
+from tkinter import filedialog
+import os
 
+class GUI:
+    def __init__(self, master):
+        self.master = master
+        self.myinit(self.master)
 
+    def myinit(self, master):
+        master.title("A simple GUI")
+
+        self.label = tk.Label(master, text="This is our first GUI!")
+        self.label.pack()
+
+        self.greet_button = tk.Button(master, text="Choose a file", command=self.choose_file)
+        self.greet_button.pack(side=tk.LEFT)
+
+        self.close_button = tk.Button(master, text="Close", command=master.quit)
+        self.close_button.pack(side=tk.RIGHT)
+
+    def greet(self):
+        print("Greetings!")
+
+    def choose_file(self):
+        path = tk.filedialog.askopenfilename(
+            initialdir = "/",
+            title = "Select file",
+            filetypes = (("jpeg files","*.jpg"),("all files","*.*"))
+        )
+        print(path)
+
+root = tk.Tk()
+my_gui = GUI(root)
+root.mainloop()
